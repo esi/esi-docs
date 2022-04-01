812f3e0a8be1e0b1e3ed3178a04f09306100108f
ErikKalkoken <erik.kalkoken@gmail.com>
2022-04-01 15:49:55 +0200

    Retrieve jwks URI from endpoint, factor out constants

--- examples/python/sso/validate_jwt.py
+++ examples/python/sso/validate_jwt.py
@@ -1,4 +1,4 @@
-"""Validates a given JWT token originating from the EVE SSO.
+"""Validates a given JWT access token originating from the EVE SSO.
 
 Prerequisites:
     * Have a Python 3 environment available to you (possibly by using a
@@ -9,7 +9,7 @@ This can be run by doing
 
 >>> python validate_jwt.py
 
-and passing in a JWT token that you have retrieved from the EVE SSO.
+and passing in a JWT access token that you have retrieved from the EVE SSO.
 """
 import sys
 
@@ -17,59 +17,73 @@ import requests
 from jose import jwt
 from jose.exceptions import ExpiredSignatureError, JWTError
 
+SSO_META_DATA_URL = "https://login.eveonline.com/.well-known/oauth-authorization-server"
+JWK_ALGORITHM = "RS256"
+JWK_ISSUERS = ("login.eveonline.com", "https://login.eveonline.com")
+JWK_AUDIENCE = "EVE Online"
 
-def validate_eve_jwt(jwt_token: str) -> dict:
-    """Validate a JWT token retrieved from the EVE SSO.
+
+def validate_eve_jwt(token: str) -> dict:
+    """Validate a JWT access token retrieved from the EVE SSO.
 
     Args:
-        jwt_token: A JWT token originating from the EVE SSO
+        token: A JWT access token originating from the EVE SSO
     Returns:
-        The contents of the validated JWT token if there are no validation errors
+        The contents of the validated JWT access token if there are no errors
     """
+    # fetch JWKs URL from meta data endpoint
+    res = requests.get(SSO_META_DATA_URL)
+    res.raise_for_status()
+    data = res.json()
+    try:
+        jwks_uri = data["jwks_uri"]
+    except KeyError:
+        raise RuntimeError(
+            f"Invalid data received from the SSO meta data endpoint: {data}"
+        ) from None
+
     # fetch JWKs from endpoint
-    res = requests.get("https://login.eveonline.com/oauth/jwks")
+    res = requests.get(jwks_uri)
     res.raise_for_status()
     data = res.json()
     try:
         jwk_sets = data["keys"]
-    except KeyError as e:
-        print(
-            "Something went wrong when retrieving the JWK set. "
-            f"The returned payload did not have the expected key {e}.\n"
-            f"Payload returned from the SSO looks like: {data}"
-        )
-        sys.exit(1)
+    except KeyError:
+        raise RuntimeError(
+            f"Invalid data received from the the jwks endpoint: {data}"
+        ) from None
 
-    # pick the JWK with the RS256 alogorithm
-    jwk_set = [item for item in jwk_sets if item["alg"] == "RS256"].pop()
+    # pick the JWK with the requested alogorithm
+    jwk_set = [item for item in jwk_sets if item["alg"] == JWK_ALGORITHM].pop()
 
     # try to decode the token and validate it against expected values
-    # will raise exceptions if decoding fails or expected values do not match
-    jwt_token = jwt.decode(
-        jwt_token,
-        jwk_set,
+    # will raise JWT exceptions if decoding fails or expected values do not match
+    contents = jwt.decode(
+        token=token,
+        key=jwk_set,
         algorithms=jwk_set["alg"],
-        issuer=("login.eveonline.com", "https://login.eveonline.com"),
-        audience="EVE Online",
+        issuer=JWK_ISSUERS,
+        audience=JWK_AUDIENCE,
     )
-    return jwt_token
+    return contents
 
 
 def main():
-    """Manually input a JWT token to be validated."""
-
     token = input("Enter an access token to validate: ")
 
     try:
-        validated_jwt = validate_eve_jwt(token)
+        token_contents = validate_eve_jwt(token)
     except ExpiredSignatureError:
         print("The JWT token has expired")
         sys.exit(1)
     except JWTError as e:
         print(f"The JWT token was invalid: {e}")
         sys.exit(1)
-
-    print(f"\nThe contents of the access token are: {validated_jwt}")
+    except RuntimeError as e:
+        print(str(e))
+        sys.exit(1)
+    else:
+        print(f"\nThe contents of the access token are:\n{token_contents}")
 
 
 if __name__ == "__main__":
